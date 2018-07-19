901baa6c433981cab697bd38786ec6664912df5c
rudifa <rudi.farkas@gmail.com>
2018-07-20 00:13:06 +0200

    add constants and unary operations

--- Calculator/Brain.swift
+++ Calculator/Brain.swift
@@ -12,16 +12,29 @@ struct Brain {
 
     private var accumulator: Double?
 
+    private enum Operation {
+        case constant(Double)
+        case unaryOp((Double) -> Double)
+    }
+
+    private var operations: Dictionary<String, Operation> = [
+        "π": .constant(Double.pi),
+        "e": .constant(M_E),
+        "√": .unaryOp(sqrt),
+        "cos": .unaryOp(cos),
+        "±": .unaryOp({-$0})
+   ]
+
     mutating func performOperation(_ symbol: String) {
-        switch symbol {
-        case "π":
-            accumulator = Double.pi
-        case "√":
-            if let operand = accumulator {
-                accumulator = sqrt(operand)
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOp(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
             }
-        default:
-            break
         }
     }
 
