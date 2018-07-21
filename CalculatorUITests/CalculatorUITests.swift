9f7107ab965691b21fafed52815c54a2f9e809ec
rudifa <rudi.farkas@gmail.com>
2018-07-21 21:40:16 +0200

    add XCUiTests

--- CalculatorUITests/CalculatorUITests.swift
+++ CalculatorUITests/CalculatorUITests.swift
@@ -31,6 +31,84 @@ class CalculatorUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+        let app = XCUIApplication()
+        print("app.state", app.state.rawValue)
+        // (lldb) po app.debugDescription
+
+        let display = app.staticTexts["AID_display"]
+        XCTAssert(display.exists)
+
+        XCTAssertEqual(display.label, "0")
+        app.buttons["1"].tap()
+        app.buttons["2"].tap()
+        app.buttons["3"].tap()
+        XCTAssertEqual(display.label, "123")
+        app.buttons["+"].tap()
+        app.buttons["4"].tap()
+        app.buttons["5"].tap()
+        app.buttons["6"].tap()
+        XCTAssertEqual(display.label, "456")
+        app.buttons["="].tap()
+        XCTAssertEqual(display.label, "579.0")
+        app.buttons["C"].tap()
+        XCTAssertEqual(display.label, "0")
+
+        XCTAssert(true)
     }
-    
+
+    func testExample2() {
+
+        let app = XCUIApplication()
+        print("app.state", app.state.rawValue)
+
+        let buttons = app.buttons
+        XCTAssertEqual(buttons.count, 24)
+
+        let texts = app.staticTexts
+        XCTAssertEqual(texts.count, 1)
+        let display = texts["AID_display"]
+
+        app.buttons["1"].tap()
+        app.buttons["5"].tap()
+        app.buttons["9"].tap()
+        XCTAssertEqual(display.label, "159")
+        app.buttons["="].tap()
+        XCTAssertEqual(display.label, "159.0")
+        app.buttons["C"].tap()
+        XCTAssertEqual(display.label, "0")
+
+        XCTAssert(true)
+    }
+
+
+    func testExample3() {
+
+        let app = XCUIApplication()
+        let display = app.staticTexts["AID_display"]
+        XCTAssertEqual(display.label, "0")
+
+        app.buttons["1"].tap()
+        app.buttons["+"].tap()
+        app.buttons["2"].tap()
+        app.buttons["="].tap()
+        XCTAssertEqual(display.label, "3.0")
+    }
+
+
+    func testExample4() {
+
+        let app = XCUIApplication()
+        let display = app.staticTexts["AID_display"]
+        XCTAssertEqual(display.label, "0")
+
+        app.buttons["."].tap()
+        app.buttons["1"].tap()
+        app.buttons["+"].tap()
+        app.buttons["."].tap()
+        app.buttons["2"].tap()
+        app.buttons["="].tap()
+        XCTAssertEqual(display.label, "0.3")
+    }
+
 }
